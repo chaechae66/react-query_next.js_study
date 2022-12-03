85bb6cec6ffb72b9b4cdbdfead551fabb3a70b2b
chaechae <dokailove6326@gmail.com>
2022-12-03 10:50:01 +0900

    Design : Footer.tsx 디자인 수정

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,7 +1,20 @@
 export default function Footer() {
     return(
-        <footer>
-            Copyright © 2022 Chae Chae,Ltd. All rights reserved.
-        </footer>
+        <>
+            <footer>
+                Copyright © 2022 Chae Chae,Ltd. All rights reserved.
+            </footer>
+            <style jsx>{`
+                footer {
+                    width:100%;
+                    height:80px;
+                    color: #ccc;
+                    background-color: #f1f1f1;
+                    display:flex;
+                    align-items:center;
+                    justify-content:center;
+                }
+            `}</style>
+        </>
     )
 }
