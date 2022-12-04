f3c9d31bc72ca6b5f7250bdd94a1136122af5883
chaechae <dokailove6326@gmail.com>
2022-12-04 16:24:29 +0900

    Feat : ListItem 구현

---
+++ components/ListItem.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+
+interface ListProps {
+    item : List
+}
+
+export default function ListItem ({ item } : ListProps) {
+    return(
+        <>
+            <div className="list_item" key={item.id}>
+                <h4>{item.name}</h4>
+                <div className="list_item_img">
+                <Image src={item.image_link} alt={item.name} width={100} height={100}/>
+                </div>
+                <div className="list_item_price">
+                {item.price}$
+                </div>
+            </div>
+            <style jsx>{`
+                h4{
+                height:45px;
+                }
+                .list_item{
+                width : 400px;
+                padding: 20px;
+                }
+                .list_item_img{
+                width:100px;
+                height:120px;
+                margin-left:100px;
+                display:flex;
+                align-items:center;
+                }
+                .list_item_price{
+                width:100%;
+                color:pink;
+                font-size:16px;
+                text-align:center;
+                font-weight:700;
+                margin-bottom:40px;
+                }
+            `}</style>
+        </>
+    )
+}
