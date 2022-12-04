3e0459fb61be960e88dc8ef35e9d996e67b05345
chaechae <dokailove6326@gmail.com>
2022-12-04 16:24:16 +0900

    Feat : ReactQuery getStaticProps에 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Header from '../components/Layout/Header'
-import Footer from '../components/Layout/Footer'
+import React from 'react'
 import Layout from '../components/Layout/Layout'
+import { QueryClientProvider, QueryClient,Hydrate } from '@tanstack/react-query'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = React.useState(() => new QueryClient())
   return (
     <>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </Hydrate>
+      </QueryClientProvider>
     </>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,28 @@
 import { getList } from "../api/cosmeticAPI"
-import { useEffect, useState } from 'react'
+import { dehydrate, QueryClient, useQuery } from '@tanstack/react-query';
 import Banner from "../components/Banner"
-import Image from "next/image"
+import ListItem from "../components/ListItem";
 
 export default function Home() {
-  const [list, setList] = useState<List[] | null>(null)
+  const { status, data, isSuccess, error } = useQuery({ queryKey: ['Lists'], queryFn: getList })
 
-  useEffect(()=>{
-    const fetchListData = async () => {
-      const listdata = await getList();
-      console.log('listdata',listdata)
-      setList(listdata)
-    }
+  if(status === "loading") {
+    return <div>로딩 중</div>
+  }
 
-    fetchListData();
-  },[])
+  if(status === "error" && error instanceof Error) {
+    return <div>{`다음과 같은 문제가 발생하였습니다. ${error.message}`}</div>
+  }
 
   return (
     <>
       <Banner />
       <section>
         {
-          !list ? (
-            <>로딩 중</>
-          ) : (<>{
-            list.map((item)=>{
+          isSuccess && (<>{
+            data.map((item)=>{
               return(
-                <div className="list_item" key={item.id}>
-                  <h4>{item.name}</h4>
-                  <div className="list_item_img">
-                    <Image src={item.image_link} alt={item.name} width={100} height={100}/>
-                  </div>
-                  <div className="list_item_price">
-                    {item.price}$
-                    </div>
-                </div>
+                <ListItem key={item.id} item={item}/>
               )
             })
           }</>)
@@ -50,29 +38,19 @@ export default function Home() {
           display:flex;
           flex-wrap:wrap;
         }
-        h4{
-          height:45px;
-        }
-        .list_item{
-          width : 400px;
-          padding: 20px;
-        }
-        .list_item_img{
-          width:100px;
-          height:120px;
-          margin-left:100px;
-          display:flex;
-          align-items:center;
-        }
-        .list_item_price{
-          width:100%;
-          color:pink;
-          font-size:16px;
-          text-align:center;
-          font-weight:700;
-          margin-bottom:40px;
-        }
       `}</style>
     </>
   )
+}
+
+export async function getStaticProps() {
+  const queryClient = new QueryClient()
+
+  await queryClient.prefetchQuery(['Lists'], getList)
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  }
 }
