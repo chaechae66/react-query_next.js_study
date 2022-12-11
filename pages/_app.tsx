9a5da544546352911ab82fdc132faccceee2d081
chaechae <valueworkerchae@gmail.com>
2022-12-11 19:25:06 +0900

    Feat : staleTime 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,13 @@ import Layout from '../components/Layout/Layout'
 import { QueryClientProvider, QueryClient,Hydrate } from '@tanstack/react-query'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [queryClient] = React.useState(() => new QueryClient())
+  const [queryClient] = React.useState(() => new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 20,
+      },
+    },
+  }))
   return (
     <>
       <QueryClientProvider client={queryClient}>
